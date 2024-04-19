df90f72c7a6af0c4abd80ea9408d42ce2e2de931
loty2019 <lorenzo.orio@icloud.com>
2024-04-18 22:33:45 -0400

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./**/*.html", // Include all HTML files in your project
+    "./**/*.js",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
